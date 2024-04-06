1551cf2049136e008421e0cdf59ac7e6aba7ba6f
suryabk <surya27bk@gmail.com>
2024-04-06 12:23:11 +0700

    Tugas 10 - Defer, Panic, Recover, Error, Goroutine dan Channel

--- Pekan2/formative-10/main.go
+++ Pekan2/formative-10/main.go
@@ -1,8 +1,93 @@
 package main
 
+import (
+	"fmt"
+	"sort"
+	"time"
+)
+
 func main() {
 	// Soal 1
 
 	// Soal 2
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Panic recovered:", r)
+		}
+	}()
+	fmt.Println(kelilingSegitigaSamaSisi(4, true))
+	fmt.Println(kelilingSegitigaSamaSisi(8, false))
+	fmt.Println(kelilingSegitigaSamaSisi(0, true))
+	fmt.Println(kelilingSegitigaSamaSisi(0, false))
+	fmt.Printf("\n\n")
 
+	// Soal 3
+	angka := 1
+
+	defer cetakAngka(&angka)
+	tambahAngka(7, &angka)
+	tambahAngka(6, &angka)
+	tambahAngka(-1, &angka)
+	tambahAngka(9, &angka)
+	fmt.Printf("\n\n")
+
+	// Soal 4
+	var phones []string
+
+	addPhones(&phones, "Xiaomi", "Asus", "IPhone", "Samsung", "Oppo", "Realme", "Vivo")
+	sort.Strings(phones)
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
+	i := 1
+	for range ticker.C {
+		if i <= len(phones) {
+			fmt.Printf("%d. %s\n", i, phones[i-1])
+			i++
+		} else {
+			break
+		}
+	}
+	fmt.Printf("\n\n")
+	// Soal 6
+
+	var movies = []string{"Harry Potter", "LOTR", "SpiderMan", "Logan", "Avengers", "Insidious", "Toy Story"}
+	moviesChannel := make(chan string)
+	go getMovies(moviesChannel, movies...)
+	for value := range moviesChannel {
+		fmt.Println(value)
+	}
+
+}
+
+func kelilingSegitigaSamaSisi(sisi int, description bool) string {
+	if sisi == 0 {
+		errMsg := "Maaf anda belum menginput sisi dari segitiga sama sisi\n"
+		return errMsg
+
+	}
+	keliling := sisi * 3
+	if description {
+		return fmt.Sprintf("keliling segitiga sama sisinya dengan sisi %d cm adalah %d cm\n", sisi, keliling)
+	} else {
+		return fmt.Sprintf("%d\n", keliling)
+	}
+}
+
+func tambahAngka(nilai int, total *int) {
+	*total += nilai
+}
+
+func cetakAngka(total *int) {
+	fmt.Println("Total :", *total)
+}
+
+func getMovies(ch chan<- string, movies ...string) {
+	for _, movie := range movies {
+		ch <- movie
+	}
+	close(ch)
+}
+
+func addPhones(phones *[]string, data ...string) {
+	*phones = append(*phones, data...)
 }
