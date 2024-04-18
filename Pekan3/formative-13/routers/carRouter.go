65e4411fdaee0552608344c4a6b5679157e3e8e3
suryabk <surya27bk@gmail.com>
2024-04-18 11:50:36 +0700

    Tugas 13 - Gin Framework

---
+++ Pekan3/formative-13/routers/carRouter.go
@@ -0,0 +1,21 @@
+package routers
+
+import (
+	"formative-13/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func StartServer() *gin.Engine {
+	router := gin.Default()
+
+	router.POST("/cars", controllers.CreateCar)
+
+	router.PUT("/cars/:carID", controllers.UpdateCar)
+
+	router.GET("/cars/:carID", controllers.GetCar)
+
+	router.DELETE("/cars/:carID", controllers.DeleteCar)
+
+	return router
+}
